079c1137ed9e7bc940c6ad8ea0b5f5180085d4a6
Екатерина <kgluhova49@gmail.com>
2023-02-09 16:21:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 8000;
+
+          int bonus = TicketPrice / 20;
+
+          System.out.println("Бонус:" + bonus);
+    }
+}
